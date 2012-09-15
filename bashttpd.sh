7e274ac91b78311f34d26fa57a1833e5e9f4e598
Roger Pate <roger@qxxy.com>
2012-09-15 11:19:25 -0400

    simple changes got out of hand

--- bashttpd.sh
+++ bashttpd.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 # A simple HTTP server written in bash.
-#
-# Bashttpd will serve text files, and most binaries as Base64 encoded.
-#
 # Avleen Vig, 2012-09-13
-#
-# 
 
 if [ "$(id -u)" = "0" ]; then
    echo "Hold on, tiger! Don't run this as root, k?" 1>&2
@@ -14,56 +9,38 @@ if [ "$(id -u)" = "0" ]; then
 fi
 
 
-DOCROOT=/var/www/html
+DOCROOT=${DOCROOT:-/var/www/html}
 
-DATE=$( date +"%a, %d %b %Y %H:%M:%S %Z" )
-REPLY_HEADERS="Date: ${DATE}
-Expires: ${DATE}
-Server: Slash Bin Slash Bash"
+REPLY_HEADERS=(
+  "Date: $(date --rfc-2822)"
+  "Expires: $(date --rfc-2822 --date='+5 hours')"
+  "Server: bashttpd"
+  )
 
-function get_content_type() {
-    URL_PATH=$1
-    CONTENT_TYPE=$( file --mime-type ${URL_PATH} | awk '{ print $2 }' )
+function send_header {
+    echo "HTTP/1.0 $*"$'\r'
+    for x in "${REPLY_HEADERS[@]}"; do
+      echo "$x"$'\r'
+    done
+    echo $'\r'
+    #sed 's/$/\r/' <<<"HTTP/1.0 $*"$'\n'"$REPLY_HEADERS"
 }
 
-function get_content_body() {
-    URL_PATH=$1
-    CONTENT_TYPE=$2
-    if [[ ${CONTENT_TYPE} =~ "^text" ]]; then
-        CONTENT_BODY="$( cat ${URL_PATH} )"
-    else
-        CONTENT_BODY="$( cat ${URL_PATH} )"
-    fi
-}
-
-function get_content_length() {
-    CONTENT_BODY="$1"
-    CONTENT_LENGTH=$( echo ${CONTENT_BODY} | wc -c )
-}
-
-while read line; do
-    # If we've reached the end of the headers, break.
-    echo ${line} | grep -v '^GET' > /dev/null
-    if [ $? -eq 0 ]; then
-        break
-    fi
-
-    # Look for a GET request
-    echo ${line} | grep ^GET > /dev/null
-    if [ $? -eq 0 ]; then
-        URL_PATH="${DOCROOT}$( echo ${line} | awk '{print $2}' )"
-        URL_PATH=$( echo ${URL_PATH} | tr -d '\r' )
-    fi
-done
-
-[[ "$URL_PATH" == *..* ]] && echo "HTTP/1.0 400 Bad Request\rn";echo "${REPLY_HEADERS}"; exit 
-
-# If URL_PATH isn't set, return 400
-if [ -z "${URL_PATH}" ]; then
-    echo "HTTP/1.0 400 Bad Request"
-    echo "${REPLY_HEADERS}"
-    echo
+function bad_request {
+    send_header 400 "Bad Request"
     exit
+}
+
+IFS=' ' read HTTP_METHOD URL_PATH HTTP_VER || bad_request
+[[ GET == $HTTP_METHOD ]] || bad_request
+URL_PATH="$DOCROOT$URL_PATH"
+
+# If URL_PATH contains
+#   leading "." in a component,
+#   "%",
+# or isn't set, return 400.
+if [[ "$URL_PATH" == */.* || "$URL_PATH" == *%* || -z "${URL_PATH}" ]]; then
+    bad_request
 fi
 
 # Check the URL requested.
@@ -72,40 +49,23 @@ fi
 # If it's a directory, perform an "ls -la".
 # Otherwise, return a 404.
 if [ -f ${URL_PATH} -a -r ${URL_PATH} ]; then
-    # Return 200 and file contents
-    get_content_type "${URL_PATH}"
-    get_content_body "${URL_PATH}" "${CONTENT_TYPE}"
-    get_content_length "${CONTENT_BODY}"
-    HTTP_RESPONSE="HTTP/1.0 200 OK"
+    exec 3<"$URL_PATH"
+    REPLY_HEADERS+=("Content-type: $(file --brief --mime-type "$URL_PATH")")
+    REPLY_HEADERS+=("Content-length: $(stat -c%s "$URL_PATH")")
 elif [ -f ${URL_PATH} -a ! -r ${URL_PATH} ]; then
-    # Return 403 for unreadable files
-    echo "HTTP/1.0 403 Forbidden"
-    echo "${REPLY_HEADERS}"
-    echo
+    send_header 403 Forbidden
     exit
-elif [ -d ${URL_PATH} ]; then
-    # Return 200 for directory listings
-    CONTENT_TYPE="text/plain"
-    CONTENT_BODY=$( ls -la ${URL_PATH} )
-    CONTENT_LENGTH=$(echo "${CONTENT_BODY}" | wc -c)
-    HTTP_RESPONSE="HTTP/1.0 200 OK"
-elif [ -d ${URL_PATH} -a ! -x ${URL_PATH} ]; then
-    # Return 403 for non-listable directories
-    echo "HTTP/1.0 403 Forbidden"
-    echo "${REPLY_HEADERS}"
-    echo
-    exit
+elif [ -d "${URL_PATH}" ]; then
+    if [ ! -x "${URL_PATH}" ]; then
+        send_header 403 Forbidden
+        exit
+    fi
+    exec 3< <(ls -l "$URL_PATH")
+    REPLY_HEADERS+=("Content-type: text/plain")
 else
-    echo "HTTP/1.0 404 Not Found"
-    echo "${REPLY_HEADERS}"
-    echo
+    send_header 404 "Not Found"
     exit
 fi
 
-echo -n "${HTTP_RESPONSE}"
-echo "${REPLY_HEADERS}"
-#echo "Content-length: ${CONTENT_LENGTH}"
-echo "Content-type: ${CONTENT_TYPE}"
-echo
-echo "${CONTENT_BODY}"
-exit
+send_header 200 OK
+cat <&3
